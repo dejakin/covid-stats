01aac44c88cb448c8c3d104b65143240ba065aa9
github username <github email address>
2020-10-27 12:20:07 +0000

    Add counting animation to values
    react-countup package allows a counting animation before the final value is rendered

--- src/components/Cards/Cards.js
+++ src/components/Cards/Cards.js
@@ -1,6 +1,7 @@
 import React, { Fragment } from 'react';
 import { Card, CardContent, Typography, Grid } from '@material-ui/core';
 import Spinner from '../Layout/Spinner';
+import CountUp from 'react-countup';
 
 import classes from './Cards.module.css';
 
@@ -9,7 +10,6 @@ const Cards = ({ data: { confirmed, recovered, deaths } }) => {
     /* If the stats function from App.js has not returned, a spinner will be rendered. 
     Once the data is fetched and in the state, the card components with the data will
     be rendered instead */
-
     return (
         <Fragment>
             {!confirmed ? <Spinner /> :  
@@ -18,7 +18,7 @@ const Cards = ({ data: { confirmed, recovered, deaths } }) => {
                     <Grid item component={Card}>
                         <CardContent>
                             <Typography color="textSecondary" gutterBottom>Confirmed</Typography>
-                            <Typography variant="h5">{confirmed.value}</Typography>
+                            <Typography variant="h5"><CountUp start={0} end={confirmed.value} duration={1} separator=',' /></Typography>
                             <Typography color="textSecondary">Date</Typography>
                             <Typography variant="body2">Number of confirmed cases</Typography>
                         </CardContent>
@@ -26,7 +26,7 @@ const Cards = ({ data: { confirmed, recovered, deaths } }) => {
                     <Grid item component={Card}>
                         <CardContent>
                             <Typography color="textSecondary" gutterBottom>Recovered</Typography>
-                            <Typography variant="h5">{recovered.value}</Typography>
+                            <Typography variant="h5"><CountUp start={0} end={recovered.value} duration={1} separator=',' /></Typography>
                             <Typography color="textSecondary">Date</Typography>
                             <Typography variant="body2">Number of recoveries</Typography>
                         </CardContent>
@@ -34,7 +34,7 @@ const Cards = ({ data: { confirmed, recovered, deaths } }) => {
                     <Grid item component={Card}>
                         <CardContent>
                             <Typography color="textSecondary" gutterBottom>Deaths</Typography>
-                            <Typography variant="h5">{deaths.value}</Typography>
+                            <Typography variant="h5"><CountUp start={0} end={deaths.value} duration={1} separator=',' /></Typography>
                             <Typography color="textSecondary">Date</Typography>
                             <Typography variant="body2">Number of deaths</Typography>
                         </CardContent>
