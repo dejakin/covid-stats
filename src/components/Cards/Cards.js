3cf140ed571d301aeee8163370a512d2ab796948
github username <github email address>
2020-10-27 13:13:45 +0000

    Add responsive attributes to cards
    Size of cards is adjusted depending on size of screen

--- src/components/Cards/Cards.js
+++ src/components/Cards/Cards.js
@@ -5,7 +5,7 @@ import CountUp from 'react-countup';
 
 import classes from './Cards.module.css';
 
-const Cards = ({ data: { confirmed, recovered, deaths } }) => {
+const Cards = ({ data: { confirmed, recovered, deaths, lastUpdate } }) => {
 
     /* If the stats function from App.js has not returned, a spinner will be rendered. 
     Once the data is fetched and in the state, the card components with the data will
@@ -15,28 +15,25 @@ const Cards = ({ data: { confirmed, recovered, deaths } }) => {
             {!confirmed ? <Spinner /> :  
             <div className={classes.container}>
                 <Grid container spacing={3} justify="center">
-                    <Grid item component={Card}>
+                    <Grid item component={Card} xs={12} md={3} className={classes.card}>
                         <CardContent>
                             <Typography color="textSecondary" gutterBottom>Confirmed</Typography>
                             <Typography variant="h5"><CountUp start={0} end={confirmed.value} duration={1} separator=',' /></Typography>
-                            <Typography color="textSecondary">Date</Typography>
-                            <Typography variant="body2">Number of confirmed cases</Typography>
+                            <Typography color="textSecondary">as of {new Date(lastUpdate).toDateString()}</Typography>
                         </CardContent>
                     </Grid>
-                    <Grid item component={Card}>
+                    <Grid item component={Card} xs={12} md={3} className={classes.card}>
                         <CardContent>
                             <Typography color="textSecondary" gutterBottom>Recovered</Typography>
                             <Typography variant="h5"><CountUp start={0} end={recovered.value} duration={1} separator=',' /></Typography>
-                            <Typography color="textSecondary">Date</Typography>
-                            <Typography variant="body2">Number of recoveries</Typography>
+                            <Typography color="textSecondary">as of {new Date(lastUpdate).toDateString()}</Typography>
                         </CardContent>
                     </Grid>
-                    <Grid item component={Card}>
+                    <Grid item component={Card} xs={12} md={3} className={classes.card}>
                         <CardContent>
                             <Typography color="textSecondary" gutterBottom>Deaths</Typography>
                             <Typography variant="h5"><CountUp start={0} end={deaths.value} duration={1} separator=',' /></Typography>
-                            <Typography color="textSecondary">Date</Typography>
-                            <Typography variant="body2">Number of deaths</Typography>
+                            <Typography color="textSecondary">as of {new Date(lastUpdate).toDateString()}</Typography>
                         </CardContent>
                     </Grid>
                 </Grid>
