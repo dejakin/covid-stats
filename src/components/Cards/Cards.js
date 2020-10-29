96995432d2ec9afb857a550b6e930634674f441d
github username <github email address>
2020-10-29 21:49:11 +0000

    Add coloured borders to the bottom of 'recovered' and 'deaths' cards

--- src/components/Cards/Cards.js
+++ src/components/Cards/Cards.js
@@ -2,10 +2,13 @@ import React, { Fragment } from 'react';
 import { Card, CardContent, Typography, Grid } from '@material-ui/core';
 import Spinner from '../Layout/Spinner';
 import CountUp from 'react-countup';
+import cx from 'classnames';
 
 import classes from './Cards.module.css';
 
 const Cards = ({ data: { confirmed, recovered, deaths, lastUpdate } }) => {
+
+    
 
     /* If the stats function from App.js has not returned, a spinner will be rendered. 
     Once the data is fetched and in the state, the card components with the data will
@@ -15,21 +18,21 @@ const Cards = ({ data: { confirmed, recovered, deaths, lastUpdate } }) => {
             {!confirmed ? <Spinner /> :  
             <div className={classes.container}>
                 <Grid container spacing={3} justify="center">
-                    <Grid item component={Card} xs={12} md={3} className={classes.card}>
+                    <Grid item component={Card} xs={12} md={3} className={classes.confirmed}>
                         <CardContent>
                             <Typography color="textSecondary" gutterBottom>Confirmed</Typography>
                             <Typography variant="h5"><CountUp start={0} end={confirmed.value} duration={1} separator=',' /></Typography>
                             <Typography color="textSecondary">as of {new Date(lastUpdate).toDateString()}</Typography>
                         </CardContent>
                     </Grid>
-                    <Grid item component={Card} xs={12} md={3} className={classes.card}>
+                    <Grid item component={Card} xs={12} md={3} className={cx(classes.card, classes.recovered)}>
                         <CardContent>
                             <Typography color="textSecondary" gutterBottom>Recovered</Typography>
                             <Typography variant="h5"><CountUp start={0} end={recovered.value} duration={1} separator=',' /></Typography>
                             <Typography color="textSecondary">as of {new Date(lastUpdate).toDateString()}</Typography>
                         </CardContent>
                     </Grid>
-                    <Grid item component={Card} xs={12} md={3} className={classes.card}>
+                    <Grid item component={Card} xs={12} md={3} className={cx(classes.card, classes.deaths)}>
                         <CardContent>
                             <Typography color="textSecondary" gutterBottom>Deaths</Typography>
                             <Typography variant="h5"><CountUp start={0} end={deaths.value} duration={1} separator=',' /></Typography>
