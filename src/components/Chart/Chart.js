6783e28eb8f9ef76c65b756a3d5eab8c808205f0
github username <github email address>
2020-11-02 13:01:54 +0000

    Create line graph to show trend in cases/deaths
    react-chartjs-2 library is used to create a line graph which maps through each object in the state array and uses each reportDate as a label, with confirmed.total and deaths.total as the datasets. A check is made on the state first to see if the fetchApi function has returned before mapping through it and rendering the chart.

--- src/components/Chart/Chart.js
+++ src/components/Chart/Chart.js
@@ -1,19 +1,42 @@
 import React, { useState, useEffect } from 'react';
 import { getDailyData } from '../../api/index';
+import { Line } from 'react-chartjs-2';
+import classes from './Chart.module.css';
 
 const Chart = () => {
-    const [dailyData, setDailyData] = useState({});
+    const [dailyData, setDailyData] = useState([]);
     useEffect(() => {
         const fetchApi = async () => {
-            const result = await getDailyData(); 
-            setDailyData({ data: result });
+            setDailyData(await getDailyData());
         }
-
         fetchApi();
     }, [])
 
+const lineChart = (
+    /* Check to see if fetchApi function has returned first otherwise there will be
+    no data in the state to map through */
+    !dailyData[0] ? null : 
+    ( <Line data={{
+        labels: dailyData.map((stat) => stat.reportDate),
+        datasets: [{
+            data: dailyData.map((stat) => stat.confirmed.total),
+            label: 'Confirmed',
+            borderColor: '#3333ff',
+            fill: true
+        }, {
+            data: dailyData.map((stat) => stat.deaths.total),
+            label: 'Deaths',
+            borderColor: 'red',
+            backgroundColor: 'rgba(255, 0, 0, 0.5)',
+            fill: true
+        }]
+    }} /> )
+) 
+
     return (
-        <h1>Chart</h1>
+        <div className={classes.container}>
+            {lineChart}
+        </div>
     )
 }
 
